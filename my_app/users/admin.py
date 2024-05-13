fb662fabcd8abaaf044333aa49a40409f47890a9
kasia-joanna-nowak <nowak.katarzyna.joanna@gmail.com>
2024-05-13 11:00:59 +0200

    create view and connect it with urls

--- my_app/users/admin.py
+++ my_app/users/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, NewTask
 
+
+admin.site.register(NewTask)
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
--- my_app/users/urls.py
+++ my_app/users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("<int:task_id>/", views.task_detail, name="task_detail"),
+    path("<int:task_id>/status",views.task_status, name="task_status")
 ]
