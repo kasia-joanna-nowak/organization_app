f1890fcde0c65eedb33e56b1cc34810c3106bf9d
kasia-joanna-nowak <nowak.katarzyna.joanna@gmail.com>
2024-05-14 08:51:55 +0200

    change in urls and models in users

--- my_app/my_app/urls.py
+++ my_app/my_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
+    # path('', include('users.urls'))
     path('admin/', admin.site.urls),
     path("todo/", include("users.urls"))
 ]
--- my_app/users/models.py
+++ my_app/users/models.py
@@ -21,11 +21,3 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
-class NewTask(models.Model):
-    task_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-    def __str__(self):
-        return self.task_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
