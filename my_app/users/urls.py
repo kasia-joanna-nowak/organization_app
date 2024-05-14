360bfa2a52bb15d975a822b6e9334060263ea8ab
kasia-joanna-nowak <nowak.katarzyna.joanna@gmail.com>
2024-05-14 08:51:27 +0200

    delete views and urls from users

--- my_app/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("<int:task_id>/", views.task_detail, name="task_detail"),
-    path("<int:task_id>/status",views.task_status, name="task_status")
-]
