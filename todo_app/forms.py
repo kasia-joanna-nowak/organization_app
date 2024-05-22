e580f01ca8f65a99f7017176e2e30596b9fca9f7
kasia-joanna-nowak <nowak.katarzyna.joanna@gmail.com>
2024-05-22 14:25:49 +0200

    create form to save input in database

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,19 +1,27 @@
 from django import forms
 from .models import NewTask
+from django.forms import ModelForm
 
-# class NewTaskForm(forms.ModelForm):
+
+
+# class TaskForm(forms.Form):
+#     task_text = forms.TextInput()
+#     pub_date = forms.DateField()
 #     class Meta:
 #         model = NewTask
-#         fields = ["pub_date",  "task_text"]
+#         fields  = ['task_text', 'pub_date']
 
-class NewTaskForm(forms.Form):
+
+
+class StatusForm(forms.Form):
     class Meta:
         model = NewTask
         fields ="__all__"
-    # task = forms.CharField(label="Enter task", max_length=200)
-    #     status_choices =  (
-    #         ('todo', 'ToDo'),
-    #         ('in_progress', 'In Progress'),
-    #         ('done', 'Done'),
-    #     )
-    #     status = forms.ChoiceField(choices=status_choices, widget=forms.CheckboxSelectMultiple)
+    task = forms.CharField(label="Enter task", max_length=200)
+    
+    status_choices =  (
+            ('todo', 'ToDo'),
+            ('in_progress', 'In Progress'),
+            ('done', 'Done'),
+        )
+    status = forms.ChoiceField(choices=status_choices, widget=forms.CheckboxSelectMultiple)
