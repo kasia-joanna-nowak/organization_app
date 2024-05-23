d00fb66d8755b65d06d088c26a32ac5364b9394c
kasia-joanna-nowak <nowak.katarzyna.joanna@gmail.com>
2024-05-23 13:54:11 +0200

    create task model test

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,17 +1,14 @@
-# from django.test import TestCase
-# from .models import NewTask
-
-# # Create your tests here.
-# import datetime
-
-# from django.test import TestCase
-# from django.utils import timezone
+from django.test import TestCase
+from .models import NewTask
+import datetime
+from django.utils import timezone
+from django.urls import reverse
 
 
-# class TaskModelTests(TestCase):
-#     def test_if_task_was_added(self):
-#         task = NewTask()
-#         task.todo('clean the house')
-#         assert 'clean the house' in 
 
+   
+class TaskModelTests(TestCase):
+   def test_adding_task_to_table(self):
+      task = NewTask.objects.create(task_text='test')
+      response=self.client
 
