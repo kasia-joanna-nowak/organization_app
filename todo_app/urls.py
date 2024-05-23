5104e2e1cfeac9e1e65a88dd9665ede056331398
kasia-joanna-nowak <nowak.katarzyna.joanna@gmail.com>
2024-05-23 13:52:24 +0200

    patch change

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -7,6 +7,7 @@ app_name = "todo_app"
 
 urlpatterns = [
     path('', views.todo, name='todo'),
-    path('<int:task_id>/', views.task_detail),
-    path('add/', views.add_task, name='add_task')
+    path('todo/<int:task_id>/', views.task_detail),
+    path('add/', views.add_task, name='add_task'),
+   
 ]
